10787374b7194eb89b1db61ff647ffaa1b3a17c6
Hoon Wee Meng <hoonweemeng@gmail.com>
2025-02-03 00:05:00 +0800

    make recipe variale nullable

--- src/model/base/Recipe.php
+++ src/model/base/Recipe.php
@@ -5,32 +5,32 @@ namespace model\base;
 
 class Recipe
 {
-    public string $recipeId;
-    public string $title;
-    public string $description;
-    public int $duration;
-    public int $servings;
-    public string $ingredients;
-    public string $instructions;
+    public ?string $recipeId;
+    public ?string $title;
+    public ?string $description;
+    public ?int $duration;
+    public ?int $servings;
+    public ?string $ingredients;
+    public ?string $instructions;
     public ?string $recipeImage;
     public ?string $recipeImageExt;
-    public \DateTime $timestamp;
-    public string $userId;
-    public int $likes;
+    public ?\DateTime $timestamp;
+    public ?string $userId;
+    public ?int $likes;
 
     public function __construct(
-        string $recipeId,
-        string $title,
-        string $description,
-        int $duration,
-        int $servings,
-        string $ingredients,
-        string $instructions,
+        ?string $recipeId,
+        ?string $title,
+        ?string $description,
+        ?int $duration,
+        ?int $servings,
+        ?string $ingredients,
+        ?string $instructions,
         ?string $recipeImage,
         ?string $recipeImageExt,
-        \DateTime $timestamp,
-        string $userId,
-        int $likes
+        ?\DateTime $timestamp,
+        ?string $userId,
+        ?int $likes
     ) {
         $this->recipeId = $recipeId;
         $this->title = $title;
